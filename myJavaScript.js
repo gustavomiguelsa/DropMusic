9256c26f4e1b1dae33e809beb39fa69730e376f1
Bruno Ferreira <brunomfferreira@hotmail.com>
2018-11-13 17:54:40 +0000

    Still working on the Login Form

--- myJavaScript.js
+++ myJavaScript.js
@@ -1,3 +1,15 @@
+document.getElementById('login_button').addEventListener('click', function(){
+  document.querySelector('.bg-modal').style.display = "flex";
+
+
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+  docment.querySelector('.bg-modal').style.display = "none";
+
+});
+
+
 function myFunction() {
     
 	return true;
