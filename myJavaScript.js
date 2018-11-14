44b43759e041da0abdf0cde0278ebfff0140d17f
Bruno Ferreira <brunomfferreira@hotmail.com>
2018-11-14 22:55:25 +0000

    Further work on the register

--- myJavaScript.js
+++ myJavaScript.js
@@ -1,13 +1,23 @@
-document.getElementById('button').addEventListener("click", function() {
-	
-});
+function registerFunction(){
+		console.log("teste1");
+		var username_=document.getElementById("in_username_reg").value;
+		var birthday_=document.getElementById("in_ddn_reg").value;
+		console.log(username_);
+		console.log(birthday_);
+		$.ajax({
+			url:'/register.php',
+			type:'POST',
+			dataType: 'json',
+			data:{
+				b: birthday_,
+				u: username_
+			}
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
-});
 
-function myFunction() {
-    
-	return true;
-}
+		}).done(function(data){
+
+			console.log("teste2");			
+		});
+	}
+
 
